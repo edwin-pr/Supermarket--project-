7dd6664c77ce613bdbddc52927394e96429263e2
WanjiruKinyua <maureentally@gmail.com>
2023-05-27 16:08:09 -0700

    update

--- shell.c
+++ shell.c
@@ -1,24 +1,49 @@
 #include "myshell.h"
 /**
- * prompt - display prompt
+ * execute_builtin - Execute a built-in command
+ * @args: Array of command arguments
  */
-void prompt(void)
+void execute_builtin(char **args)
 {
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "$ ", strlen("$ "));
+	if (strcmp(args[0], "exit") == 0)
+		exit_shell();
+	else if (strcmp(args[0], "cd") == 0)
+		change_directory(args);
+	else if (strcmp(args[0], "env") == 0)
+		print_environment();
 }
 /**
- * sigint_handler - handles the signals to shell
- * @signal: input signal
- * Return: prompt on success
+ * execute_command - Execute an external command
+ * @args: Array of command arguments
  */
-
-void sigint_handler(int signal)
+void execute_command(char **args)
 {
-	if (signal == SIGINT)
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
 	{
-		write(STDOUT_FILENO, "\n", 1);
-		prompt();
-		fflush(stdout);
+		if (execve(args[0], args, NULL) == -1)
+			perror("execve");
+		exit(EXIT_FAILURE);
 	}
+	else if (pid < 0)
+	{
+		perror("fork");
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+}
+/**
+ *  main - Entry point of the shell program
+ *  Return: 0 on success, or error code
+ */
+int main(void)
+{
+	return (start_shell());
 }
